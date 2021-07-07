1c008773ebb83ff8081479ca7dae631dd994d5d9
Tyler Koch <ttkochdev@gmail.com>
2021-07-07 12:04:08 -0400

    connecting database and displaying results raw and unfiltered in table. db info is not git ignored for project clearity

---
+++ db.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+$servername = "localhost";
+$username = "root";
+$password = "password";
+$dbname = "sweetwater_test";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+  echo "<br>" . $e->getMessage();
+}
+
+?>
--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
-    echo "hello world";
+require "db.php";
+    
+
+$sql = $conn->prepare("SELECT * FROM sweetwater_test");
+$sql->execute();
+
+if($sql->rowCount()) {
+?>
+<table border="0">
+    <tr COLSPAN=2 BGCOLOR="#6D8FFF">
+        <td>Order ID</td>
+        <td>Comments</td>
+        <td>shipdate_expected</td>
+    </tr>
+<?php
+while($row = $sql->fetch(PDO::FETCH_ASSOC)) {
+?>
+    <tr>
+        <td><?php print($row['orderid']); ?></td>
+        <td><?php print($row['comments']); ?></td>
+        <td><?php print($row['shipdate_expected']); ?></td>
+    </tr>
+<?php
+}
+?>
+</table>
+<?php
+} else {
+
+    print('No results found');
+
+}
 ?>
